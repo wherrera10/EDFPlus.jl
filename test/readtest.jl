df987e1976ea33318fe269ddb74b3360332aa676
Bill <wherrera10@users.noreply.github.com>
2017-12-30 16:32:35 -1000

    add tests of struct

--- test/readtest.jl
+++ test/readtest.jl
@@ -1,5 +1,17 @@
 using EDFPlus
+using Base.Test
 
 
+edfh = loadfile("EDFPlusTestFile.edf")
 
+gen = edfh.gender
+achan = edfh.annotationchannel
+typ = edfh.filetype
 
+closefile(edfh)
+
+@test gen == "Female"
+@test achan == 30
+@test typ == FileStatus(1)
+
+true
