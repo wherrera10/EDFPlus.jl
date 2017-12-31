1073f66f76bccffdcf754c30678b40eae9f80ebd
Bill <wherrera10@users.noreply.github.com>
2017-12-30 16:33:38 -1000

    add test of struct size after write

--- test/writetest.jl
+++ test/writetest.jl
@@ -1,3 +1,12 @@
 using EDFPlus
+using Base.Test
 
 
+edfh = loadfile("EDFPlusTestFile.edf")
+sz = size(edfh.EDFsignals)
+@test sz == (20010,601)
+
+newedfh = writefile(edfh, "NEWedfplustestfile.edf")
+@test size(newedfh.EDFsignals) == sz
+
+true
