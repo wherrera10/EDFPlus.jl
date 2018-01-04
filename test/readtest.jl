9a75e523a8d3cb7766a34e2819acf5449d10463c
Bill <wherrera10@users.noreply.github.com>
2018-01-04 00:43:25 -1000

    add imap tests

--- test/readtest.jl
+++ test/readtest.jl
@@ -4,14 +4,35 @@ using Base.Test
 
 edfh = loadfile("EDFPlusTestFile.edf")
 
-gen = edfh.gender
-achan = edfh.annotationchannel
-typ = edfh.filetype
+@test edfh.gender == "Female"
+@test edfh.annotationchannel == 30
+@ test edfh.filetype == FileStatus(1)
+
+fs = samplerate(edfh,1)
+f1 = EDFPlus.recordslice(edfh, 21, 22)[1,:]
+f2 = highpassfilter(reshape(f1, length(f1)), fs)
+f3 = lowpassfilter(f2, fs)
+f4 = notchfilter(f3, fs)
+@test round(f4[end-3],2) == 0.01
+
+eegpages = epoch_iterator(edfh, 12, channels=[7,8,9])
+annots = annotation_epoch_iterator(edfh, 12)
+
+for (pgnum, page) in enumerate(eegpages)
+    if pgnum == 1
+        @test round(page[3][end], 3) == -24.121
+    elseif pgnum == 30
+        @test round(page[1][100], 3) ==  -10.84
+    end
+end
+
+for (i, ann) in enumerate(annots)
+    if i == 9
+        @test ann[1][1].onset == 96.0
+    end
+end
 
 closefile(edfh)
-
-@test gen == "Female"
-@test achan == 30
-@test typ == FileStatus(1)
+@test edfh.filetype == FileStatus(6)
 
 true
