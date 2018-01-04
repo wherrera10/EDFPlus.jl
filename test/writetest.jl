c5988c8f57d18cbdf23aec1442f6fc8f3a3ae76e
Bill <wherrera10@users.noreply.github.com>
2018-01-04 00:44:00 -1000

    add annotation change test

--- test/writetest.jl
+++ test/writetest.jl
@@ -6,7 +6,12 @@ edfh = loadfile("EDFPlusTestFile.edf")
 sz = size(edfh.EDFsignals)
 @test sz == (20010,601)
 
+ann = Annotation(61.04, "", "NotedThis")
+addannotation(edfh, ann.onset, ann.duration, ann.annotation)
+
+
 newedfh = writefile(edfh, "NEWedfplustestfile.edf")
 @test size(newedfh.EDFsignals) == sz
+
 
 true
