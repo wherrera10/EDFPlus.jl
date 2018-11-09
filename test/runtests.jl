956d7c222f2d6644f4b68fb5ad7bb89cf1bc2746
Bill <wherrera10@users.noreply.github.com>
2018-11-08 19:44:18 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using EDFPlus
 using Test
 # Run tests
 
-@elapsed begin
-    @time @test include("readtest.jl")
-    @time @test include("writetest.jl")
-end
+@test include("readtest.jl")
+@test include("writetest.jl")
+
+
