3033d8bc668ab6f65055124c60154208f8d00f25
Bill <wherrera10@users.noreply.github.com>
2019-05-24 10:49:15 -1000

    Update cover.jl

--- test/cover.jl
+++ test/cover.jl
@@ -4,14 +4,15 @@ using Test
 
 edfh = loadfile("EDFPlusTestFile.edf")
 
-EDFPlus.version()
-epoch_iterator(edfh, 1)
-annotation_epoch_iterator(edfh, 1)
-channeltimesegment(edfh, 1, 0.5, 1.5, true)
-multichanneltimesegment(edfh, [1], 0.25, 1.25, false)
-sg = physicalchanneldata(edfh, 2)
-fs = samplerate(edfh, 2)
-notchfilter(sg, fs)
-highpassfilter(sg, fs)
-highpassfilter(sg, fs)
-lowpassfilter(sg, fs)
+EDFPlus.version() != nothing
+epoch_iterator(edfh, 1) != nothing
+annotation_epoch_iterator(edfh, 1) != nothing
+channeltimesegment(edfh, 1, 0.5, 1.5, true) != nothing
+multichanneltimesegment(edfh, [1], 0.25, 1.25, false) != nothing
+sg = physicalchanneldata(edfh, 2) != nothing
+(fs = samplerate(edfh, 2)) != nothing
+notchfilter(sg, fs) != nothing
+highpassfilter(sg, fs) != nothing
+highpassfilter(sg, fs) != nothing
+lowpassfilter(sg, fs) != nothing
+
