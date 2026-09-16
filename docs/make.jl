fd7930fd923e8b9b687b0cd9993c5473f6c56049
Bill <wherrera10@users.noreply.github.com>
2026-09-16 12:31:23 -1000

    Revise makedocs and deploydocs configurations
    Updated documentation generation settings for EDFPlus.jl.

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,24 @@
 using Documenter, EDFPlus
 
-makedocs(sitename = "EDFPlus Module Documentation", format = Documenter.HTML(prettyurls = false))
-  
+DocMeta.setdocmeta!(EDFPlus, :DocTestSetup, :(using EDFPlus); recursive=true)
+
+makedocs(;
+    modules=[EDFPlus],
+    authors="William Herrera and various patch submitters",
+    sitename="EDFPlus.jl Documentation",
+    repo="github.com/wherrera10/EDFPlus.jl.git",
+    format=Documenter.HTML(;
+        canonical="https://wherrera10.github.io/EDFPlus.jl",
+        edit_link="master",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        # Add future pages here, e.g., "API Reference" => "api.md"
+    ],
+)
+
+deploydocs(;
+    repo="github.com/wherrera10/EDFPlus.jl.git",
+    devbranch="master",
+)
