2418ff24846daa824c275d4fb2f866b225b8afe2
Bill <wherrera10@users.noreply.github.com>
2026-09-16 16:02:10 -1000

    Update repository link format in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(;
     modules=[EDFPlus],
     authors="William Herrera and various patch submitters",
     sitename="EDFPlus.jl Documentation",
-    repo="https://github.com/wherrera10/EDFPlus.jl/blob/{commit}{path}#{line}",
+    repo=Documenter.Remotes.GitHub("wherrera10", "EDFPlus.jl"),
     format=Documenter.HTML(;
         canonical="https://wherrera10.github.io/EDFPlus.jl",
         edit_link="master",
